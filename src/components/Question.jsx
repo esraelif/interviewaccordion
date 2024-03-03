22c540be2238b55da69506e16e738b316017b92b
esraelif <elifesratunca@gmail.com>
2024-03-03 10:19:43 +0100

    Question.jsx

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { arrowdown } from "../helper/Icons"
+import { arrowdown, arrowup } from "../helper/Icons"
+import { useState } from 'react';
 
 
 const Question = ({ questions }) => {
-
+    const [arrow, setArrow] = useState(true)
     return (
 
-        <div className='questions'>
-            <h2>{questions.question}</h2>
-            {arrowdown}
+        <div className='questions' onClick={(setArrow(!arrow))}>
+            {arrow ? (<><h2>{questions.question}</h2><button>{arrowdown} </button></>) : (<><h2>{questions.answer}</h2><button>{arrowup}</button></>)}
+
         </div>
 
 
